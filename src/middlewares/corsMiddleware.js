00fb0209a07256f8765b20769ced907c36213fde
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2024-03-21 13:51:48 +0700

    fix: update v2 project

--- src/middlewares/corsMiddleware.js
+++
@@ -1,8 +0,0 @@
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://thedreamhome.click"], // Replace with your frontend domain
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: false, // Enable credentials (cookies, authorization headers, etc.)
-  optionsSuccessStatus: 200, // Respond with 204 (No Content) for preflight requests
-};
-
-module.exports = corsOptions;
